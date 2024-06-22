c88df88998f460a114ac02c6663813009d86fa8d
John <m.johnmarshal@gmail.com>
2024-06-22 10:34:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# audio.johninvirtual.com
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>audio.johninvirtual.com</h1>
+
+<section>
+    <h2>Pitch detector</h2>
+    <p>Implementation based on <a target="_blank" href="https://www.toptal.com/webassembly/webassembly-rust-tutorial-web-audio">webassembly-rust-tutorial-web-audio</a></p>
+</section>
